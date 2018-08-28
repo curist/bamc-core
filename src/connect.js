3dc537ad6003e3ba5144f4f51dcffa5c852d1b59
curist <curist.cyc@gmail.com>
2018-08-28 09:04:42 +0800

    using EventEmitter

--- src/connect.js
+++ src/connect.js
@@ -1,23 +1,68 @@
+import { EventEmitter } from 'events'
+
 const url = 'wss://cw2.twmuds.com/websocket/v1939'
 
-export default async function() {
+export default function() {
+  const event = new EventEmitter
   const cli = new WebSocket(url)
+
   cli.binaryType = 'arraybuffer'
 
   cli.onclose = () => {
-    console.info('conn closed')
+    console.info('conn close')
+    event.emit('close')
   }
 
-  return new Promise((resolve, reject) => {
-    cli.onerror = err => {
-      console.info('conn errored')
-      console.error(err)
-      reject(err)
-    }
+  cli.onerror = err => {
+    console.info('conn error')
+    console.error(err)
+    event.emit('error', err)
+  }
 
-    cli.onopen = () => {
-      console.log('conn opened')
-      resolve(cli)
+  cli.onopen = () => {
+    console.info('conn open')
+    event.emit('open')
+  }
+
+  cli.onmessage = e => {
+    const arraybuffer = e.data
+    const uarr = new Uint8Array(arraybuffer)
+    event.emit('message', uarr)
+  }
+
+  function cmd(cmd) {
+    raw([0xff, 0xfa, 0xc9])
+    raw(cmd)
+    raw([0xff, 0xf0]);
+  }
+
+  function send(msg) {
+    raw(`${msg}\n`)
+  }
+
+  function raw(bytes) {
+    if(typeof bytes == 'string') {
+      cli.send(bytes)
+      return
+    }
+    cli.send(new Uint8Array(bytes))
+  }
+
+  event.on('action', action => {
+    const { type } = action
+
+    switch(type) {
+      case 'send': {
+        const { message } = action
+        send(message)
+      }
+      case 'cmd': {
+        const { message } = action
+        cmd(message)
+      }
+      default: return
     }
   })
+
+  return event
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import term from './term'
 import connect from './connect'
 
-async function main() {
-  const cli = await connect()
+// IAC WILL/WONT/DO/DONT always have one following byte
+// IAC SB ends with IAC SE
+const
+  SE   = 0xF0, // end of subnegotiation
+  SB   = 0xFA, // start of subnegotiation
+  WILL = 0xFB,
+  WONT = 0xFC,
+  DO   = 0xFD,
+  DONT = 0xFE,
+  IAC  = 0xFF,  // Interpret As Command
+  GMCP = 0xC9   // GMCP sequence (decimal 201)
 
-  cli.onmessage = e => {
-    const arraybuffer = e.data
-    const uarr = new Uint8Array(arraybuffer)
+async function main() {
+  const event = connect()
+
+  event.on('open', () => console.log('cli open'))
+
+  // FIXME it's possible message's fragmented
+  event.on('message', uarr => {
     if(uarr.slice(0, 3).join(',') !== '255,250,201') {
       const text = new TextDecoder().decode(uarr)
       term.write(text)
       return
     }
-    // FIXME it's possible message's fragmented
     for(let i = 3; i < uarr.length - 1; i++) {
       if(uarr[i] == 255 && uarr[i + 1] == 240) {
         const gmcpText = new TextDecoder().decode(uarr.slice(3, i))
@@ -23,38 +35,27 @@ async function main() {
       }
     }
     console.error('gmcp parsing error')
-  }
+  })
 
-  function cmd(cmd) {
-    raw([0xff, 0xfa, 0xc9])
-    raw(cmd)
-    raw([0xff, 0xf0]);
-  }
-
-  function send(msg) {
-    raw(`${msg}\n`)
-  }
-
-  function raw(bytes) {
-    if(typeof bytes == 'string') {
-      cli.send(bytes)
-      return
-    }
-    cli.send(new Uint8Array(bytes))
-  }
 
   $('form#send').on('submit', e => {
     e.preventDefault()
     const $input = $(e.target).find('input')
     const value = $input.val()
-    send(value)
+    event.emit('action', {
+      type: 'send',
+      message: value
+    })
     $input.select()
   })
   $('form#cmd').on('submit', e => {
     e.preventDefault()
     const $input = $(e.target).find('input')
     const value = $input.val()
-    cmd(value)
+    event.emit('action', {
+      type: 'cmd',
+      message: value
+    })
     $input.select()
   })
 }
