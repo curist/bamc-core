c65dd2938d1961796f589e901154d4dca99f8f7a
curist <curist.cyc@gmail.com>
2018-09-10 01:01:05 +0800

    show things properly

--- src/connect.js
+++ src/connect.js
@@ -1,24 +1,8 @@
 import { EventEmitter } from 'events'
-
 import { TelnetInput } from 'telnet-stream'
-const telnetInput = new TelnetInput();
+import { PassThrough } from 'stream'
 
-const { PassThrough } = require('stream');
-const pass = new PassThrough();
-
-pass.pipe(telnetInput)
-
-telnetInput.on('data', data => console.log(`data ${data}`))
-telnetInput.on('do', data => console.log(`do ${data}`))
-telnetInput.on('will', data => console.log(`will ${data}`))
-telnetInput.on('command', data => console.log(`cmd ${data}`))
-telnetInput.on('sub', (...data) => console.log(`sub ${data}`))
-
-pass.on('data', chunk => console.log('chunk: ' + chunk.toString()))
-pass.write('ok')
-pass.write(new Uint8Array([0xff, 0xfd, 0xc9]))
-pass.write(new Uint8Array([0xff, 0xfa, 0xc9, 0x61, 0x62, 0x63, 0xff, 0xf0, 0x61]))
-
+const GMCP = 0xc9
 
 const url = 'wss://cw2.twmuds.com/websocket/v1939'
 // user / password
@@ -29,7 +13,21 @@ const url = 'wss://cw2.twmuds.com/websocket/v1939'
 // sync_time
 export default function() {
   const event = new EventEmitter
-  const cli = {} // new WebSocket(url)
+  const cli = new WebSocket(url)
+
+  const pass = new PassThrough()
+  const telnetInput = new TelnetInput()
+  pass.pipe(telnetInput)
+
+  telnetInput.on('data', data => event.emit('data', data))
+  telnetInput.on('do', data => console.log(`do ${data}`))
+  telnetInput.on('will', data => console.log(`will ${data}`))
+  telnetInput.on('command', data => console.log(`cmd ${data}`))
+  telnetInput.on('sub', (sub, data) => {
+    if(sub === GMCP) {
+      event.emit('gmcp', data)
+    }
+  })
 
   cli.binaryType = 'arraybuffer'
 
@@ -52,7 +50,7 @@ export default function() {
   cli.onmessage = e => {
     const arraybuffer = e.data
     const uarr = new Uint8Array(arraybuffer)
-    event.emit('message', uarr)
+    pass.write(uarr)
   }
 
   function cmd(cmd) {
--- src/index.js
+++ src/index.js
@@ -1,40 +1,22 @@
+import './style.css'
 import term from './term'
 import connect from './connect'
 
-// IAC WILL/WONT/DO/DONT always have one following byte
-// IAC SB ends with IAC SE
-const
-  SE   = 0xF0, // end of subnegotiation
-  SB   = 0xFA, // start of subnegotiation
-  WILL = 0xFB,
-  WONT = 0xFC,
-  DO   = 0xFD,
-  DONT = 0xFE,
-  IAC  = 0xFF,  // Interpret As Command
-  GMCP = 0xC9   // GMCP sequence (decimal 201)
 
 async function main() {
   const event = connect()
 
   event.on('open', () => console.log('cli open'))
 
-  // FIXME it's possible message's fragmented
-  event.on('message', uarr => {
-    if(uarr.slice(0, 3).join(',') !== '255,250,201') {
-      const text = new TextDecoder().decode(uarr)
-      term.write(text)
-      return
-    }
-    for(let i = 3; i < uarr.length - 1; i++) {
-      if(uarr[i] == 255 && uarr[i + 1] == 240) {
-        const gmcpText = new TextDecoder().decode(uarr.slice(3, i))
-        $('pre').append('\n' + gmcpText)
-        const text = new TextDecoder().decode(uarr.slice(i + 2))
-        term.write(text)
-        return
-      }
-    }
-    console.error('gmcp parsing error')
+  event.on('data', uarr => {
+    const text = new TextDecoder().decode(uarr)
+    term.write(text)
+  })
+
+  event.on('gmcp', uarr => {
+    const gmcpText = new TextDecoder().decode(uarr)
+    $('pre').append('\n' + gmcpText)
+    $('pre').scrollTop($('pre')[0].scrollHeight)
   })
 
 
